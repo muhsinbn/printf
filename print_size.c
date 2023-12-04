961069d7c6fa80917554e1e494105d9507753e73
marylambtech <marylambtech@gmail.com>
2023-12-04 15:48:02 -0800

    print_size.c

---
+++ print_size.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_size - To attain by calculating the size to get the argument
+ * @a: The list of printed arguments
+ * @format: The format that prints the argumets
+ * Print format as a string
+ * Return: The size
+ */
+
+int get_size(const char *format, int *a)
+{
+	int curr_a = *a + 1;
+	int size = 0;
+
+	if (format[curr_a] == 'l')
+		size = S_LONG;
+	else if (format[curr_a] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*a = curr_a - 1;
+	else
+		*a = curr_a;
+
+	return (size);
+}
